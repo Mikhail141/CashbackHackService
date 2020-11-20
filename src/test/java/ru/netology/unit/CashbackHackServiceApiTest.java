c05c21d16f8add3e0d689f8db1721f5d33c0d92b
Mikhail141 <70147484+Mikhail141@users.noreply.github.com>
2020-11-20 17:40:37 +0300

    new api

--- src/test/java/ru/netology/unit/CashbackHackServiceApiTest.java
+++ src/test/java/ru/netology/unit/CashbackHackServiceApiTest.java
@@ -1,9 +1,11 @@
 package ru.netology.unit;
 
-import org.junit.Test;
+
+
+import org.junit.jupiter.api.Test;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.jupiter.api.Assertions.*;
+
 
 class CashbackHackServiceApiTest {
     CashbackHackServiceApi serviceApi = new CashbackHackServiceApi();
@@ -12,6 +14,20 @@ class CashbackHackServiceApiTest {
     public void shouldAmountLess1000(){
         int actual = serviceApi.remain(880);
         int expected = 120;
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldAmountZero(){
+        int actual = serviceApi.remain(0);
+        int expected = 1000;
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldAmount1000(){
+        int actual = serviceApi.remain(1000);
+        int expected = 0;
         assertEquals(expected, actual);
     }
 
